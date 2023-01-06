c8ee54ffb02c43fe3fcdbf8f3376720983c3354e
dalia-alawneh <alawnehdalia340@gmail.com>
2023-01-06 20:32:14 +0200

    login updated

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -16,7 +16,7 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
         Text(
-          "Start or join a meeting",
+          "Start join a meeting",
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
         ),
         Padding(
