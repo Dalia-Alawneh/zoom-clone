4aacc57b6d9ec3fe56258a28e5834c08e0db770a
dalia-alawneh <alawnehdalia340@gmail.com>
2023-01-06 16:03:56 +0200

    login screen added

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:zoomclone/widgets/custom_button.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Text(
+          "Start or join a meeting",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 40.0),
+          child: Image.asset('assets/images/onboarding.jpg'),
+        ),
+        CustomButton(
+          text: 'Google Sign In',
+          onPressed: (){
+            //google sign in with firebase 
+          },
+        )
+      ]),
+    );
+  }
+}
