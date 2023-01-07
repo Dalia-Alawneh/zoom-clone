da47ce91d2fdf2feb5374058b0ab314b81a80404
mman9our <hack_225@outlook.sa>
2023-01-07 23:45:50 +0200

    New release

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -16,7 +16,7 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
         Text(
-          "Start OR4 join a meeting",
+          "Start OR222 join a meeting",
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
         ),
         Padding(
