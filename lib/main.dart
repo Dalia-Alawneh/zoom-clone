8e3e663056ce4a37ad1e68bf9a1d3245b77d5137
dalia-alawneh <alawnehdalia340@gmail.com>
2023-01-19 20:16:13 +0200

    info added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:zoomclone/resources/auth_methods.dart';
@@ -12,30 +13,39 @@ void main() async {
       title: "Zoom Clone",
       debugShowCheckedModeBanner: false,
       theme:
-          ThemeData.dark().copyWith(scaffoldBackgroundColor: backgroundColor),
+          ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: backgroundColor),
       routes: {
         '/login': (context) => LoginScreen(),
         '/home': (context) => HomeScreen(),
       },
-    home: const MyApp()));
+      home: AnimatedSplashScreen(
+        splash: 'assets/images/loading.png',
+        nextScreen: MyApp(),
+        splashTransition: SplashTransition.fadeTransition,
+        splashIconSize: 60,
+        curve: Curves.easeInOut,
+        backgroundColor: Color.fromARGB(255, 20, 20, 20),
+        
+      )));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return  StreamBuilder(
-        stream: AuthMethods().authChanges,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (snapshot.hasData) {
-            print('hhh');
-            return const HomeScreen();
-          }
-          return const LoginScreen();
-        },
+    return StreamBuilder(
+      stream: AuthMethods().authChanges,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasData) {
+          print('hhh');
+          return const HomeScreen();
+        }
+        return const LoginScreen();
+      },
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:zoomclone/utils/dialog_tile.dart';
 import 'package:zoomclone/widgets/home_meeting_button.dart';
 import '../utils/colors.dart';
 
@@ -24,6 +25,110 @@ class _HomeScreenState extends State<HomeScreen> {
         elevation: 0,
         title: Text('Meet & Chat'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            color: Colors.white,
+            icon: Icon(Icons.info_outline),
+            onPressed: () => {
+              showGeneralDialog(
+                barrierLabel: "showGeneralDialog",
+                barrierDismissible: true,
+                barrierColor: Colors.black.withOpacity(0.6),
+                transitionDuration: const Duration(milliseconds: 400),
+                context: context,
+                pageBuilder: (context, _, __) {
+                  return Align(
+                      alignment: Alignment.bottomCenter,
+                      child: IntrinsicHeight(
+                        child: Container(
+                          width: double.maxFinite,
+                          clipBehavior: Clip.antiAlias,
+                          padding: const EdgeInsets.all(16),
+                          decoration: const BoxDecoration(
+                            color: backgroundColor,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(16),
+                              topRight: Radius.circular(16),
+                            ),
+                          ),
+                          child: Material(
+                            color: Colors.transparent,
+                            child: Column(
+                              children: [
+                                Text('Personal Meeting ID',
+                                    style: TextStyle(color: textColor)),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text(
+                                  '987 475 101',
+                                  style:
+                                      TextStyle(fontSize: 26, color: textColor),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(15),
+                                  decoration: BoxDecoration(
+                                      color: secondaryBackgroundColor,
+                                      borderRadius: BorderRadius.circular(15)),
+                                  child: Column(
+                                    children: [
+                                      DialogTile(
+                                          text: 'Start Meeting',
+                                          icon: Icons.calendar_month),
+                                      Divider(
+                                        height: 30,
+                                        thickness: 0.4,
+                                        color:
+                                            Color.fromARGB(40, 143, 143, 143),
+                                      ),
+                                      DialogTile(
+                                          text: 'Send Invitation',
+                                          icon: Icons.share),
+                                      Divider(
+                                        height: 30,
+                                        thickness: 0.4,
+                                        color:
+                                            Color.fromARGB(40, 143, 143, 143),
+                                      ),
+                                      DialogTile(
+                                          text: 'Edit Meeting',
+                                          icon: Icons.edit_outlined),
+                                    ],
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 18,
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Text('Cancel',
+                                      style: TextStyle(
+                                          color: textColor, fontSize: 20)),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ));
+                },
+                transitionBuilder: (_, animation1, __, child) {
+                  return SlideTransition(
+                    position: Tween(
+                      begin: const Offset(0, 1),
+                      end: const Offset(0, 0),
+                    ).animate(animation1),
+                    child: child,
+                  );
+                },
+              ),
+            },
+          )
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: footerColor,
