2e6b4d541e1ac7a08e90f5adc53dfc84db655784
Wafa' Abdalhadi <85899817+wafaabdalhadi@users.noreply.github.com>
2023-01-20 18:01:28 +0200

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,34 +1,11 @@
-import 'package:flutter/material.dart';
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Center(
-          child: Text('home'),
-        ));
-=======
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:zoomclone/utils/dialog_tile.dart';
 import 'package:zoomclone/widgets/home_meeting_button.dart';
-=======
 import 'package:zoomclone/screens/history_meeting_screen.dart';
 import 'package:zoomclone/screens/meeting_screen.dart';
->>>>>>> wafaa
 import '../utils/colors.dart';
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -186,6 +163,5 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
     );
->>>>>>> 8e3e663056ce4a37ad1e68bf9a1d3245b77d5137
   }
 }
