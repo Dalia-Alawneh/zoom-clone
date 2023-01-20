8d8eff3577514b8db71d80e3da5583abc524c626
Wafa' Abdalhadi <85899817+wafaabdalhadi@users.noreply.github.com>
2023-01-20 17:59:57 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,11 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:zoomclone/resources/auth_methods.dart';
 import 'package:zoomclone/screens/home_screen.dart';
 import 'package:zoomclone/screens/login_screen.dart';
-import 'package:zoomclone/screens/meeting_screen.dart';
 import 'package:zoomclone/screens/video_call_screen.dart';
 import './utils/colors.dart';
 
-<<<<<<< HEAD
-
-void main() {
-  runApp(const MyApp());
-=======
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -20,29 +13,15 @@ void main() async {
       title: "Zoom Clone",
       debugShowCheckedModeBanner: false,
       theme:
-          ThemeData.dark().copyWith(
-            scaffoldBackgroundColor: backgroundColor),
+      ThemeData.dark().copyWith(scaffoldBackgroundColor: backgroundColor),
       routes: {
         '/login': (context) => LoginScreen(),
         '/home': (context) => HomeScreen(),
         '/video-call': (context) => VideoCallScreen(),
       },
-<<<<<<< HEAD
-      home: AnimatedSplashScreen(
-        splash: 'assets/images/loading.png',
-        nextScreen: MyApp(),
-        splashTransition: SplashTransition.fadeTransition,
-        splashIconSize: 60,
-        curve: Curves.easeInOut,
-        backgroundColor: Color.fromARGB(255, 20, 20, 20),
-        
-      )));
->>>>>>> 8e3e663056ce4a37ad1e68bf9a1d3245b77d5137
-=======
       home: HomeScreen()
 
   ));
->>>>>>> wafaa
 }
 
 class MyApp extends StatelessWidget {
